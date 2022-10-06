3f18dc0482c7ff89a63bb3a547c8529e64844e4c
Microgorath <connorcga@gmail.com>
2022-10-05 20:50:54 -0400

    switch to sounddevice stream, add device settings

--- liveinference_soundcard.py
+++ liveinference_soundcard.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
-from pickle import TRUE
 import librosa
 import torchaudio
 import yaml
 import noisereduce as nr
 import PySimpleGUI as sg
 import threading
-import time as Time
 
-import sounddevice as sd
-# import soundcard as sc
+# import sounddevice as sd
+import soundcard as sc
 
 from models import *
 from Utils.JDC.model import JDCNet
@@ -19,21 +17,20 @@ from Utils.JDC.model import JDCNet
 
 
 parser = argparse.ArgumentParser(description=__doc__)
-# parser.add_argument("-i", "--input-device", type=str, help="input device name", default=None)
-# parser.add_argument("-o", "--output-device", type=str, help="output device name", default=None)
+parser.add_argument("-i", "--input-device", type=str, help="input device name", default="")
+parser.add_argument("-o", "--output-device", type=str, help="output device name", default="")
 # parser.add_argument("-c", "--channels", type=int, default=1,
 #                     help="number of channels") # not currently used
 parser.add_argument("-t", "--dtype", help="audio data type")
 parser.add_argument("-s", "--samplerate", type=float, help="sampling rate", default=24000)
-parser.add_argument("-b", "--blocksize", type=int, help="block size in frames, automatically determined from latency when 0", default=3000)
-parser.add_argument("-l", "--latency", type=float, help="desired latency between input and output", default=0.5)
+parser.add_argument("-b", "--blocksize", type=int, help="block size", default=30*300)
 parser.add_argument("-w", "--wave-buffer-size", type=float, help="wave buffer size in frames", default=24000)
-parser.add_argument("-bl", "--blend-length", type=float, help="number of frames to crossfade between each block", default=50)
-parser.add_argument("-ngh", "--noise-gate-hold", type=float, help="time in ms to hold open the noise gate", default=120.0)
+parser.add_argument("-bl", "--blend-length", type=float, help="number of frames to crossfade between each block", default=300)
+parser.add_argument("-ngh", "--noise-gate-hold", type=float, help="time in ms to hold open the noise gate", default=70.0)
 parser.add_argument("-oms", "--out-mel-shift", type=int, 
-                    help="shift converted mel back by this many mels, prevents artifacts at end of converted audio", default=0)
+                    help="shift converted mel back by this many mels, prevents artifacts at end of converted audio", default=2)
 parser.add_argument("-ws", "--wave-shift", type=int, 
-                    help="shift vocoder output back by this many samples, prevents artifacts at end of vocoder output", default=1)
+                    help="shift vocoder output back by this many samples, prevents artifacts at end of vocoder output", default=300)
 parser.add_argument("-v", "--vocoder-model", type=str, help="path to vocoder model to use",
                     default="Vocoder/pretrained_PWG/checkpoint-400000steps.pkl")
 parser.add_argument("-sg", "--stargan-model", type=str, help="path to trained starganv2VC model to use", 
@@ -143,13 +140,13 @@ def compute_style(ref_path, starganv2, speaker, rng):
 # |  wave  |cut|
 #          | next wave |cut|
 #                      | next next wave |cut|
-def next_waves(wave, wave_cut, frames):
-    wave_left = wave.shape[0] - frames - args.blend_length - args.wave_shift
+def next_waves(wave, wave_cut):
+    wave_left = wave.shape[0] - args.blocksize - args.blend_length - args.wave_shift
     wave_right = wave.shape[0] - args.blend_length - args.wave_shift
     wave_return = wave[wave_left:wave_right].squeeze()
     wave_cut_next = wave[wave_right:-args.wave_shift].squeeze()
 
-    # TODO: improve blend smoothness, or find some way to do phase alignment?
+    # TODO: improve blend smoothness, or find some way to do phase alignment
     buffer_weight = np.linspace(0, 1, args.blend_length)
     wave_return[:args.blend_length] = buffer_weight * wave_return[:args.blend_length] + (1 - buffer_weight) * wave_cut
     wave_return = np.clip(wave_return, -1, 1)
@@ -178,16 +175,16 @@ ref_path = args.reference
 ref = compute_style(ref_path, starganv2, speaker, rng=rng)
 
 # Set the speaker and mic to be used for VC, use the system defaults if no device specified
-# input_device = sc.default_microphone()
-# output_device = sc.default_speaker()
-# if args.input_device != "":
-#     input_device = sc.get_microphone(args.input_device)
-# if args.output_device != "":
-#     output_device = sc.get_speaker(args.output_device)
+input_device = sc.default_microphone()
+output_device = sc.default_speaker()
+if args.input_device != "":
+    input_device = sc.get_microphone(args.input_device)
+if args.output_device != "":
+    output_device = sc.get_speaker(args.output_device)
 
 wave_buffer = np.zeros(args.wave_buffer_size) # Should this be on cuda?
 wave_cut = None
-noise = np.zeros(args.wave_buffer_size)
+noise = np.zeros(args.blocksize)
 noise_threshold = 0.0
 enable_conversion = True
 enable_noisereduction = False
@@ -195,7 +192,7 @@ enable_stationary_nr = False
 stop_audio = False
 
 # Find the minimum mel bin amplitude by processing a wave of zeroes and taking the first element
-minimum_mel = preprocess(np.zeros(args.samplerate))[0, 0, 0]
+minimum_mel = preprocess(np.zeros(args.blocksize))[0, 0, 0]
 
 # TODO: implement a proper noise gate
 # Create a filter to smoothen the silence detection with. Should be an odd number of entries.
@@ -206,7 +203,7 @@ silence_filter = torch.tensor([[[0.1, 0.2, 0.4, 0.2, 0.1]]]).to('cuda')
 silence_hold_size = int(np.ceil(args.noise_gate_hold / 1000. * args.samplerate / 300.) * 2 + 1)
 
 
-def callback(indata, outdata, frames, time, status):
+def callback(indata, block_num, time, status):
     global wave_buffer
     global wave_cut
 
@@ -224,11 +221,6 @@ def callback(indata, outdata, frames, time, status):
         buffer_cut = int(wave_buffer.shape[0] - args.wave_buffer_size)
         wave_buffer = wave_buffer[max(0, buffer_cut):, ...] # shift back by blocksize, so size is args.wave_buffer_size
 
-        # If conversion is not enabled, just pass noise reduced audio to output.
-        if not enable_conversion:
-            outdata[:] = np.expand_dims(audio, axis=1)
-            return
-
         mel = preprocess(wave_buffer).to('cuda') 
         # mel = mel[..., 1:] # omits first in 3rd dim
 
@@ -243,9 +235,9 @@ def callback(indata, outdata, frames, time, status):
         # Use a max pool layer as the 'pre-open' and 'hold' on a noise gate, with duration == the size of the kernel in frames / 2
         pooled_mel = torch.nn.functional.max_pool1d(filtered_mel, kernel_size=silence_hold_size, stride=1, padding=silence_hold_size//2)
         silence_mask = pooled_mel < noise_threshold
-        
+
         out = convert(mel, starganv2, F0_model, ref, args.speaker)
-        
+
         silence_mask = silence_mask.T[1:, ...]
         mel_silenced = out.masked_fill_(silence_mask, minimum_mel)
 
@@ -257,52 +249,50 @@ def callback(indata, outdata, frames, time, status):
         wave = wave.cpu().numpy()
         wave.dtype = np.float32
 
-        # for bug testing, save wave here for each block, with file name according to 'time' argument
-
         if wave_cut is None:
-            wave_left = wave.shape[0] - frames - args.blend_length - args.wave_shift
+            wave_left = wave.shape[0] - args.blocksize - args.blend_length - args.wave_shift
             wave_right = wave.shape[0] - args.blend_length - args.wave_shift
             wave_cut = wave[wave_right:-args.wave_shift, ...].squeeze()
             wave = wave[wave_left:wave_right, ...]
         else:
-            wave, wave_cut = next_waves(wave, wave_cut, frames)
+            wave, wave_cut = next_waves(wave, wave_cut)
 
     out = np.expand_dims(wave.squeeze(), axis=1)
-    outdata[:] = out
+    return out
+
+# sounddevice replacement
+# numframes must be two or four times smaller than the blocksize of the recorder and player
+def convert_audio():
+    global noise_threshold
+    global enable_conversion
+    global stop_audio
+    with input_device.recorder(samplerate=args.samplerate, blocksize=args.blocksize*2) as mic_recorder, \
+        output_device.player(samplerate=args.samplerate, blocksize=args.blocksize*2) as spk_player:
+        indata = np.zeros((args.blocksize, 1))
+        outdata = np.zeros((args.blocksize, 1))
+        block_num = 0
+
+        noise = mic_recorder.record(numframes=args.blocksize)[:, 0].squeeze() # [:, 0] only gets one channel
+        # Use the loudest recorded noise as the threshold for silencing audio
+        noise_threshold = torch.max(log_norm(preprocess(noise)))
+
+        print("Beginning Voice Conversion with the following settings:",
+            "\nReference RNG Seed:", rng.initial_seed())
+
+        while True:
+            block_num += 1
+            indata[:] = mic_recorder.record(numframes=args.blocksize)[:, 0:1] # [:, 0] only gets one channel
+            if enable_conversion:
+                outdata = callback(indata, block_num, 0, 0)
+            else:
+                outdata = indata
+            spk_player.play(outdata)
+            # stop audio thread when GUI window closed
+            if stop_audio:
+                break
 
 
-audio_stream = sd.Stream(samplerate=args.samplerate, blocksize=args.blocksize, channels=1, latency=args.latency, callback=callback)
-audio_devices = sd.query_devices()
-audio_hostapis = sd.query_hostapis()
-# Add in the index into each host API dictionary
-for index in range(len(audio_hostapis)):
-    audio_hostapis[index]['index'] = index
-
-# Separate audio devices into a nested list with outer list corresponding to which host API it uses, 
-# inner list is a list of input devices or list of output devices,
-# according to whether the device has any input or output channels
-input_devices = []
-output_devices = []
-for list_API in audio_hostapis:
-    input_devices_for_this_api = []
-    output_devices_for_this_api = []
-    for device_ID in list_API['devices']:
-        if audio_devices[int(device_ID)]['max_input_channels'] > 0: # this device is an input
-            # add a key and value to each device dictionary with its original index in sd.query_devices(), which sounddevice uses as an ID
-            audio_devices[int(device_ID)]['index'] = int(device_ID)
-            input_devices_for_this_api.append(audio_devices[int(device_ID)])
-        if audio_devices[int(device_ID)]['max_output_channels'] > 0: # this device is an output
-            # add a key and value to each device dictionary with its original index in sd.query_devices(), which sounddevice uses as an ID
-            audio_devices[int(device_ID)]['index'] = int(device_ID)
-            output_devices_for_this_api.append(audio_devices[int(device_ID)])
-    input_devices.append(input_devices_for_this_api)
-    output_devices.append(output_devices_for_this_api)
-
-
-audio_device_menu_layout = ['', [
-    'Host API', [str(api['name']) + '::ID  ' + str(api['index']) + '_API' for api in audio_hostapis],
-    'Input Device', [str(device['name']) + '::ID  ' + str(device['index']) + '_INPUT' for device in input_devices[sd.default.hostapi]],
-    'Output Device', [str(device['name']) + '::ID  ' + str(device['index']) + '_OUTPUT' for device in output_devices[sd.default.hostapi]]]]
+audio_thread = threading.Thread(target=convert_audio)
 
 # TODO: add a slider to add or subtract from the noise threshhold
 # TODO: add option to set the RNG seed
@@ -315,21 +305,16 @@ layout = [[sg.Button('Conversion Enabled', key='EC', button_color="green")],
             sg.Button('Record Noise', key='NR_REC'),
             sg.Checkbox('Stationary', key='NR_STAT', enable_events=True)],
         [sg.Text('Selected Voice:')],
-        [sg.Button(str(i), key='VC_' + str(i), button_color="gray") for i in range(num_speakers)],
-        [sg.ButtonMenu('Device Settings', audio_device_menu_layout, key='DEVICE_SETTINGS')]]
-        # [sg.Text('Input Device:'), sg.Combo([device['name'] for device in input_devices], key='IN_DEVICE', enable_events=True)],
-        # [sg.Text('Output Device:'), sg.Combo([device['name'] for device in output_devices], key='OUT_DEVICE', enable_events=True)]]
+        [sg.Button(str(i), key='VC_' + str(i), button_color="gray") for i in range(num_speakers)]]
 
 window = sg.Window('Voice Conversion', layout)
 
-# Start processing audio.
-audio_stream.start()
+audio_thread.start()
 
-# Start the GUI
 while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Exit'):
-        audio_stream.abort()
+        stop_audio = True
         break
     if event == 'EC':
         if enable_conversion:
@@ -355,91 +340,10 @@ while True:
     elif event == 'NR_STAT':
         enable_stationary_nr = values['NR_STAT']
     elif event == 'NR_REC':
-        noise[:] = wave_buffer
-        noise_threshold = torch.max(log_norm(preprocess(noise)))
-    elif event == 'DEVICE_SETTINGS':
-        # make sure the value of DEVICE_SETTINGS is a string before comparing it to key strings
-        if not isinstance(values['DEVICE_SETTINGS'], str):
-            print("Value of DEVICE_SETTINGS event is not a string, when it should be.")
-            break
-        
-        # Now, check which menu was selected from Host API, Input Device, or Output Device.
-        if values['DEVICE_SETTINGS'][-4:] == '_API':
-            # Not great implementation, but the menu requires values to be string
-            api_ID = int(values['DEVICE_SETTINGS'][-7:-4])
-            # When the Host API is changed, the Input Device and Output Device menus need to be updated.
-            audio_device_menu_layout = ['', [
-                'Host API', [str(api['name']) + '::ID  ' + str(api['index']) + '_API' for api in audio_hostapis],
-                'Input Device', [str(device['name']) + '::ID  ' + str(device['index']) + '_INPUT' for device in input_devices[api_ID]],
-                'Output Device', [str(device['name']) + '::ID  ' + str(device['index']) + '_OUTPUT' for device in output_devices[api_ID]]]]
-            window['DEVICE_SETTINGS'].update(audio_device_menu_layout)
-            # The current input and output devices should be changed to the default for that Host API?
-            input_ID = int(audio_hostapis[api_ID]['default_input_device'])
-            output_ID = int(audio_hostapis[api_ID]['default_output_device'])
-            # For some reason some of the default devices are invalid. Try to instead find a valid device for that host API if it's invalid.
-            try:
-                sd.check_input_settings(device = input_ID, samplerate=args.samplerate, blocksize=args.blocksize, channels=1, latency=args.latency)
-            except:
-                found_valid = False
-                for device in input_devices[api_ID]:
-                    try:
-                        input_ID = device['index']
-                        sd.check_input_settings(device = input_ID, samplerate=args.samplerate, blocksize=args.blocksize, channels=1, latency=args.latency)
-                    except:
-                        # print('input device', input_ID, 'causes exception')
-                        continue
-                    else: # If one of the devices work, then keep it and stop searching.
-                        # print('input device', input_ID, 'found that does not cause exception')
-                        found_valid = True
-                    if found_valid:
-                        break
-                if not found_valid:
-                    print('No valid input device found for this host API :(')
-            
-            try:
-                sd.check_output_settings(device = output_ID, samplerate=args.samplerate, blocksize=args.blocksize, channels=1, latency=args.latency)
-            except:
-                found_valid = False
-                for device in output_devices[api_ID]:
-                    try:
-                        output_ID = device['index']
-                        sd.check_output_settings(device = output_ID, samplerate=args.samplerate, blocksize=args.blocksize, channels=1, latency=args.latency)
-                    except:
-                        # print('output device', output_ID, 'causes exception')
-                        continue
-                    else: # If one of the devices work, then keep it and stop searching.
-                        # print('output device', output_ID, 'found that does not cause exception')
-                        found_valid = True
-                    if found_valid:
-                        break
-                if not found_valid:
-                    print('No valid output device found for this host API :(')
-                # print('output device works fine')
-            # print(input_ID, '|', output_ID)
-            
-            # Stop the audio stream and replace it with a new one that uses the specified input device, then start it back.
-            audio_stream.stop()
-            audio_stream = sd.Stream(device=(input_ID, output_ID), samplerate=args.samplerate, blocksize=args.blocksize, channels=1, latency=args.latency, callback=callback)
-            audio_stream.start()
+        with input_device.recorder(samplerate=args.samplerate, blocksize=args.blocksize*2) as mic_recorder:
+            # TODO: print this on the GUI instead
+            # print("Recording noisy audio for noise reduction, please do not speak yet...")
+            noise = mic_recorder.record(numframes=args.blocksize)[:, 0].squeeze() # [:, 0] only gets one channel
+            # print("Done recording noise!")
+            noise_threshold = torch.max(log_norm(preprocess(noise)))
 
-        elif values['DEVICE_SETTINGS'][-6:] == '_INPUT':
-            # Get the index of the device from the key string in values via a hardcoded position splice
-            input_ID = int(values['DEVICE_SETTINGS'][-9:-6])
-            # Get the current output device index to keep after stream is restarted
-            output_ID = audio_stream.device[1]
-            
-            # Stop the audio stream and replace it with a new one that uses the specified input device, then start it back.
-            audio_stream.stop()
-            audio_stream = sd.Stream(device=(input_ID, output_ID), samplerate=args.samplerate, blocksize=args.blocksize, channels=1, latency=args.latency, callback=callback)
-            audio_stream.start()
-
-        elif values['DEVICE_SETTINGS'][-7:] == '_OUTPUT':
-            # Get the index of the device from the key string in values via a hardcoded position splice
-            output_ID = int(values['DEVICE_SETTINGS'][-10:-7])
-            # Get the current input device index to keep after stream is restarted
-            input_ID = audio_stream.device[0]
-            
-            # Stop the audio stream and replace it with a new one that uses the specified input device, then start it back.
-            audio_stream.stop()
-            audio_stream = sd.Stream(device=(input_ID, output_ID), samplerate=args.samplerate, blocksize=args.blocksize, channels=1, latency=args.latency, callback=callback)
-            audio_stream.start()
